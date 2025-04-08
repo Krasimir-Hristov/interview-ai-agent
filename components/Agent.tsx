1e5d791c7cd10a6843a9ae675f5abe3bf832e44b
krasi <krasimir.xristov@gmail.com>
2025-04-08 13:43:05 +0200

    feat: implement Agent component and integrate interview data retrieval

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -5,6 +5,7 @@ import { cn } from '@/lib/utils';
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import { vapi } from '@/lib/vapi.sdk';
+import { interviewer } from '@/app/constants';
 
 enum CallStatus {
   INACTIVE = 'INACTIVE',
@@ -18,9 +19,14 @@ interface SavedMessage {
   content: string;
 }
 
-const Agent = ({ userName, userId, type }: AgentProps) => {
+const Agent = ({
+  userName,
+  userId,
+  type,
+  interviewId,
+  questions,
+}: AgentProps) => {
   const router = useRouter();
-
   const [isSpeaking, setIsSpeaking] = useState(false);
   const [callStatus, setCallStatus] = useState<CallStatus>(CallStatus.INACTIVE);
   const [messages, setMessages] = useState<SavedMessage[]>([]);
@@ -32,6 +38,7 @@ const Agent = ({ userName, userId, type }: AgentProps) => {
     const onMessage = (message: Message) => {
       if (message.type === 'transcript' && message.transcriptType === 'final') {
         const newMessage = { role: message.role, content: message.transcript };
+
         setMessages((prev) => [...prev, newMessage]);
       }
     };
@@ -39,10 +46,7 @@ const Agent = ({ userName, userId, type }: AgentProps) => {
     const onSpeechStart = () => setIsSpeaking(true);
     const onSpeechEnd = () => setIsSpeaking(false);
 
-    const onError = (error: Error) => {
-      console.error('Error:', error);
-      // setCallStatus(CallStatus.FINISHED);
-    };
+    const onError = (error: Error) => console.log('Error', error);
 
     vapi.on('call-start', onCallStart);
     vapi.on('call-end', onCallEnd);
@@ -61,32 +65,70 @@ const Agent = ({ userName, userId, type }: AgentProps) => {
     };
   }, []);
 
+  const handleGenerateFeedback = async (messages: SavedMessage[]) => {
+    console.log('Generate feedback here.');
+
+    // TODO: Create a server action that generates feedback
+    const { success, id } = {
+      success: true,
+      id: 'feedback-id',
+    };
+
+    if (success && id) {
+      router.push(`/interview/${interviewId}/feedback`);
+    } else {
+      console.log('Error saving feedback');
+      router.push('/');
+    }
+  };
+
   useEffect(() => {
     if (callStatus === CallStatus.FINISHED) {
-      router.push('/');
+      if (type === 'generate') {
+        router.push('/');
+      } else {
+        handleGenerateFeedback(messages);
+      }
     }
   }, [messages, callStatus, type, userId]);
 
   const handleCall = async () => {
     setCallStatus(CallStatus.CONNECTING);
 
-    await vapi.start(process.env.NEXT_PUBLIC_VAPI_WORKFLOW_ID!, {
-      variableValues: {
-        username: userName,
-        userid: userId,
-      },
-    });
+    if (type === 'generate') {
+      await vapi.start(process.env.NEXT_PUBLIC_VAPI_WORKFLOW_ID!, {
+        variableValues: {
+          username: userName,
+          userid: userId,
+        },
+      });
+    } else {
+      let formattedQuestions = '';
+
+      if (questions) {
+        formattedQuestions = questions
+          .map((question) => `- ${question}`)
+          .join('\n');
+      }
+
+      await vapi.start(interviewer, {
+        variableValues: {
+          questions: formattedQuestions,
+        },
+      });
+    }
   };
 
   const handleDisconnect = async () => {
     setCallStatus(CallStatus.FINISHED);
+
     vapi.stop();
   };
 
   const latestMessage = messages[messages.length - 1]?.content;
-
   const isCallInactiveOrFinished =
     callStatus === CallStatus.INACTIVE || callStatus === CallStatus.FINISHED;
+
   return (
     <>
       <div className='call-view'>
@@ -143,7 +185,7 @@ const Agent = ({ userName, userId, type }: AgentProps) => {
               )}
             />
 
-            <span>{isCallInactiveOrFinished ? 'Call' : '. . . '}</span>
+            <span>{isCallInactiveOrFinished ? 'Call' : '. . .'}</span>
           </button>
         ) : (
           <button className='btn-disconnect' onClick={handleDisconnect}>
